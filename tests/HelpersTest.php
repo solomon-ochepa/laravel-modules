7b0ad96b01e15b3a8502e937306559be727f1077
Solomon Ochepa <solomonochepa@gmail.com>
2024-07-24 09:56:49 +0100

    Optimize HelpersTest()

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -7,33 +7,28 @@ use Nwidart\Modules\Contracts\RepositoryInterface;
 
 class HelpersTest extends BaseTestCase
 {
-    /**
-     * @var \Illuminate\Filesystem\Filesystem
-     */
-    private $finder;
-
-    /**
-     * @var string
-     */
-    private $modulePath;
-
     public function setUp(): void
     {
         parent::setUp();
-        $this->finder = $this->app['files'];
+
         $this->createModule();
-        $this->modulePath = $this->getModuleAppPath();
     }
 
     public function tearDown(): void
     {
         $this->app[RepositoryInterface::class]->delete('Blog');
+
         parent::tearDown();
     }
 
     public function test_it_finds_the_module_path()
     {
         $this->assertTrue(Str::contains(module_path('Blog'), 'modules/Blog'));
+    }
+
+    public function test_it_finds_the_module_app_path()
+    {
+        $this->assertTrue(Str::contains(module_app_path('Blog'), 'modules/Blog/app'));
     }
 
     public function test_it_can_bind_a_relative_path_to_module_path()
